e43083d7c8277d43eeda550dae82eadb4c6b6d86
IngParker <amerchancaina@gmail.com>
2026-05-06 09:39:47 -0500

    Kalman_bye

--- ELS-ELH/ELS/main_obc_sfw/src/flight_computer.cpp
+++ ELS-ELH/ELS/main_obc_sfw/src/flight_computer.cpp
@@ -4,16 +4,12 @@
 #include "sensors_manager.h"    
 #include "error_warning.h"      
 #include "signals.h"
-#include "linear_kalman_nav.h"
 #include "flash_storage.h"    
 #include <Arduino.h>
 #include <SD.h>
 #include <math.h>
 #include <string.h>
 #include <stdio.h>
-#include "linear_kalman_nav.h"
-
-KalmanFilterNav NavFilter;
 
 static FlightState gState = STATE_INIT;
 static uint32_t gFlashWriteAddr = 0;
@@ -187,29 +183,6 @@ void flight_computer_update() {
     static float    lastLandedAlt  = 0.0f;
 
     uint32_t now = millis();
-
-/*
----------- Verify implementation - Where to implement the dt time
-    ReadMPU6050();
-    ReadBME280();
-
-    float accelZ = mpuData.MPU_az - 9.81f; 
-    NavFilter.predict(accelZ, dt);
-
-    //implement an if function for new baro data
-    float altitud_baro = bmeData.altitude;
-    NavFilter.update(altitud_baro);
-    
-    float filtered_altitude = filtroNav.getAltitude();
-    float filtered_velocity = filtroNav.getVelocity();
-
-    if (vel_actual > 280.0f) {
-        kfNav.setRBaro(1000.0f); 
-    } 
-    else {
-        kfNav.setRBaro(2.0f); 
-    }
-*/
 
     switch (gState) {
 
